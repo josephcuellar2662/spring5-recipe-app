c197e86581d6131feb583607adf7dd4cf4808b92
josephcuellar <josephcuellar2662@gmail.com>
2019-11-25 21:22:02 -0700

    Fixed bi-direction issue with lombok and entities

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,15 +1,11 @@
 package guru.springframework.controllers;
 
-import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.HashSet;
-import java.util.Set;
 
 
 @Slf4j
@@ -26,9 +22,8 @@ public class IndexController {
     @RequestMapping({"", "/", "index"})
     public String getIndexPage(Model model) {
 
-        Set<Recipe> recipes = recipeService.getRecipes();
-        model.addAttribute("recipes", recipes);
-        log.debug("recipe service: " + recipes);
+        log.debug("Inside Index Controller");
+        model.addAttribute("recipes", recipeService.getRecipes());
 
         return "index";
     }
