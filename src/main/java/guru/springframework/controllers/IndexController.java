e3e66616dd83f230ac79090cbd1fd00aa292c2b9
josephcuellar <josephcuellar2662@gmail.com>
2019-11-25 20:51:56 -0700

    Refactored Recipe Bootstrap using lombok

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,12 +1,18 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashSet;
+import java.util.Set;
 
+
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -20,7 +26,9 @@ public class IndexController {
     @RequestMapping({"", "/", "index"})
     public String getIndexPage(Model model) {
 
-        model.addAttribute("recipes", recipeService.getRecipes());
+        Set<Recipe> recipes = recipeService.getRecipes();
+        model.addAttribute("recipes", recipes);
+        log.debug("recipe service: " + recipes);
 
         return "index";
     }
--- src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -2,12 +2,14 @@ package guru.springframework.services;
 
 import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -19,8 +21,11 @@ public class RecipeServiceImpl implements RecipeService {
     }
 
     public Set<Recipe> getRecipes() {
+
+        log.debug("I'm in the service");
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
+
         return recipeSet;
     }
 }
